ce6df213c118eac59c433db4d982222df030ea93
Dan Brooks <mrdanbrooks@gmail.com>
2013-09-18 20:34:07 -0400

    [db] working example!

--- Served.py
+++ Served.py
@@ -1,12 +1,62 @@
 #!/opt/local/bin/python
 # TODO: help - return all possible options
 from SocketServer import *
+import threading
 import sys
 
 
 class Served(object):
     def __init__(self):
-        pass 
+        print "Initializing Server"
+        # List of client connections to make sure we close properly
+        self.connections = list()
+
+        # The socket server (listens for new connections)
+        try:
+            self.server = ThreadingTCPServer(('127.0.0.1',9090),ConnectionHandler)
+        except:
+            print "Could not initialize the server. The port may already be in use"
+
+        self.server.served = self # Add a reference to this object
+
+        # Run the server on a seperate thread
+#        self.running = True;
+#        self.server_thread = threading.Thread(target=self._conn_listener)
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+    def __del__(self):
+        self.shutdown()
+
+    def shutdown(self):
+#        self.running = False;
+        print "Cleaning up server process"
+        self.server.shutdown()
+
+#     def _conn_listener(self):
+#         """ Listens for incomming TCP connections """
+#         try:
+# #        server.serve_forever()
+#             print "listening"
+#             while self.running:
+#                 # Keep track of connections
+#                 request, addr = self.server.get_request()
+#                 self.server.process_request(request,addr)
+#                 self.connections.append(request)
+#         except:
+#             pass
+#         finally:
+#             print "shutting down"
+#             self.server.server_close()
+# 
+#             # Close down connection threads
+#             print "closing %d connections"%(len(self.connections))
+#             for c in self.connections:
+#                 self.server.close_request(c)
+#         print "Shutdown complete"
+# 
+
 
     def _exe(self,text):
         # Turn text string into list
@@ -29,8 +79,7 @@ class Served(object):
                 (text[0],method.func_code.co_argcount-1,len(text)-1," ".join(text[1:])))
 
         # Execute the thing
-        ret = method(*text[1:])
-        print "Ret = '%s'"%str(ret)
+        return method(*text[1:])
 
 # Creates a Threaded TCP Server
 class ThreadingTCPServer(ThreadingMixIn, TCPServer): pass
@@ -39,17 +88,21 @@ class ThreadingTCPServer(ThreadingMixIn, TCPServer): pass
 class ConnectionHandler(StreamRequestHandler):
     def setup(self):
         print "setup connection"
+        print "served=",self.server.served
         self.running = True
 
     def handle(self):
         print "handling"
         conn = self.request
-        # TODO Fix closign this connection
+        # TODO Fix closing this connection smoothly
+        # TODO Fix text buffer size 1024 overflows
         while self.running:
             data = conn.recv(1024)
             if data == "":
                 continue
-            print ">",data
+            result = self.server.served._exe(data)
+            if result:
+                conn.send(str(result)+"\n")
         print "ending"
 
     def finish(self):
@@ -62,22 +115,4 @@ if __name__ == "__main__":
     print "one"
     connections = list()
     server = ThreadingTCPServer(('127.0.0.1',9090),ConnectionHandler)
-    try:
-#        server.serve_forever()
-        print "listening"
-        while True:
-            # Keep track of connections
-            request, addr = server.get_request()
-            server.process_request(request,addr)
-            connections.append(request)
-    except:
-        print "shutting down"
-        server.server_close()
-
-        # Close down connection threads
-        print "closing %d connections"%(len(connections))
-        for c in connections:
-            server.close_request(c)
-    print "two"
-
 
--- test.py
+++ test.py
@@ -1,16 +1,48 @@
 #!/opt/local/bin/python
+# [db] Dan Brooks
+# mrdanbrooks@gmail.com
+#
+# Served - Example Use Class
+# In this example we have a simple class we will turn into a server object. 
+# class Test():
+#   foo() 
+#   bar(arg1,arg2) 
+# 
+# In a terminal window, run this example served class
+#   $ ./test.py
+#
+# In a second window, run a simple client
+#   $ nc 127.0.0.1 9090
+#   foo
+#   bar one two
+#   baz
+
+
+## Step 1: Import Served
 from Served import *
 
+# Step 2: Make your class a subclass of Served
 class Test(Served):
     def __init__(self):
+        print "Initializing Test Object"
         self.baz = "baz2"
+
+        # Step 3: If you have a constructor, make sure you init super!
+        super(Test,self).__init__()
 
     def foo(self):
         return "foo2"
 
-    def bar(self,text1,text2):
-        print "bar =",text1,text2
+    def bar(self,arg1,arg2):
+        print "bar =",arg1,arg2
 
 if __name__ == "__main__":
+    # Step 4: Instanciate your class!
     t = Test()
-    
+
+    print "press return to end"
+    raw_input()
+
+    # TODO: Figure out for sure if this line is option 
+    # (We really want it to be)
+    t.shutdown()
